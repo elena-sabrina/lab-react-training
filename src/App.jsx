ad946c44ae79266a088049312d9989e021b5754d
Elena Hadjimina <hadjimina.elena@gmail.com>
2021-03-03 21:36:54 +0800

    iteration 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import IdCard from './Components/IdCard';
 import Greetings from './Components/Greetings';
+import Random from './Components/Random';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,6 +11,8 @@ function App() {
       <IdCard />
       <Greetings lang="de">Ludwig</Greetings>
       <Greetings lang="fr">François</Greetings>
+      <Random min={1} max={6} />
+      <Random min={1} max={100} />
     </div>
   );
 }
---
+++ src/Components/Random.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Random = ({ max }) => {
+  let min = 1;
+  let random = Math.floor(Math.random() * (max - min + 1)) + min;
+  return (
+    <div>
+      <h3>
+        Random value between 1 and {max} => {random}
+      </h3>
+    </div>
+  );
+};
+
+export default Random;
